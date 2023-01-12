2b6397325f521ef7f9ee9da51d696049a625ae3e
ACHAGHOUR ZAKARIA <zakaria.achaghour@printconcept.com>
2023-01-12 10:06:15 +0100

    WIP: api gateway

---
+++ src/test/java/com/zakaria/Apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakaria.Apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
